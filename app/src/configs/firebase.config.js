57dee8e21a0e0cff9e1ecd609573da6ac70f10e7
ricky romero <contact@romeroricky.com>
2021-01-15 12:03:34 -0800

    testing env in prod

--- app/src/configs/firebase.config.js
+++ app/src/configs/firebase.config.js
@@ -1,3 +1,4 @@
+console.log(process.env.REACT_APP_FIREBASE_APP_ID);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
